09c35baa8dedabeb460ede7f9d2e0a400293419b
sadik061 <sadik061@gmail.com>
2018-03-10 20:32:19 +0600

    index file is replaced by login file code

--- index.php
+++ index.php
@@ -1,75 +1,82 @@
-<?php include 'pages/templates/head.html'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
 
 <body>
-  <div id="wrapper">
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<?php
-    include 'pages/templates/headermenu.html';
-    include 'pages/templates/sidemenu.html';
-?>
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
 
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
 
-<div id="page-wrapper">
-  <div class="row">
-      <div class="col-lg-12">
-          <h1 class="page-header">Forms</h1>
-      </div>
-      <!-- /.col-lg-12 -->
-  </div>
-  <!-- /.row -->
-  <div class="row">
-      <div class="col-lg-12">
-          <div class="panel panel-default">
-              <div class="panel-heading">
-                  Basic Form Elements
-              </div>
-              <div class="panel-body">
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
 
-
-                          <h1>Input Groups</h1>
-                          <form role="form">
-                              <div class="form-group input-group">
-                                  <span class="input-group-addon">@</span>
-                                  <input type="text" class="form-control" placeholder="Username">
-                              </div>
-                              <div class="form-group input-group">
-                                  <input type="text" class="form-control">
-                                  <span class="input-group-addon">.00</span>
-                              </div>
-                              <div class="form-group input-group">
-                                  <span class="input-group-addon"><i class="fa fa-eur"></i>
-                                  </span>
-                                  <input type="text" class="form-control" placeholder="Font Awesome Icon">
-                              </div>
-                              <div class="form-group input-group">
-                                  <span class="input-group-addon">$</span>
-                                  <input type="text" class="form-control">
-                                  <span class="input-group-addon">.00</span>
-                              </div>
-                              <div class="form-group input-group">
-                                  <input type="text" class="form-control">
-                                  <span class="input-group-btn">
-                                      <button class="btn btn-default" type="button"><i class="fa fa-search"></i>
-                                      </button>
-                                  </span>
-                              </div>
-                          </form>
-
-
-  <!-- /.row -->
-  </div>
-  <!-- /#page-wrapper -->
-</div>
-</div>
-</div>
-
-
-<?php include 'pages/templates/footer.html'; ?>
-
-
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
 
 </body>
+
 </html>
