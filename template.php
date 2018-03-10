1441eb30a22685548a58909e99be54f37514ef07
sadik061 <sadik061@gmail.com>
2018-03-10 20:28:44 +0600

    template file

---
+++ template.php
@@ -0,0 +1,30 @@
+<?php include 'pages/templates/head.html'; ?>
+
+
+
+<body>
+  <div id="wrapper">
+
+
+<?php
+    include 'pages/templates/headermenu.html';
+    include 'pages/templates/sidemenu.html';
+?>
+
+
+<div id="page-wrapper">
+
+//Add your code here
+
+
+
+</div
+</div>
+
+
+<?php include 'pages/templates/footer.html'; ?>
+
+
+
+</body>
+</html>
