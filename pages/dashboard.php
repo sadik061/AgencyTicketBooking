6285828a528b21cc269f9a11a5d1d7ad543e83cf
sadik061 <sadik061@gmail.com>
2018-03-10 20:59:53 +0600

    all demo and template files are moved to pages/templates

---
+++ pages/dashboard.php
@@ -0,0 +1,30 @@
+<?php include 'pages/templates/head.html'; ?>
+
+
+
+<body>
+  <div id="wrapper">
+
+
+<?php
+    include 'pages/templates/headermenu.html';
+    include 'pages/templates/sidemenu.html';
+?>
+
+
+<div id="page-wrapper">
+
+<h1>Add your code here</h1>
+
+
+
+</div
+</div>
+
+
+<?php include 'pages/templates/footer.html'; ?>
+
+
+
+</body>
+</html>
