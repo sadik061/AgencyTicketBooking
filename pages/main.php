d4b8f2c19a8243ae84c144d77ca3fde92595fcb0
sadik061 <sadik061@gmail.com>
2018-03-15 12:25:43 +0600

    form update 1

--- pages/main.php
+++ pages/main.php
@@ -29,9 +29,12 @@
   }
 
   ?>
+
+<div class="panel panel-default">
   <div class="col-lg-12">
                       <h1 class="page-header">Add New Entry</h1>
                   </div>
+  <div class="panel-body">
 <div class="col-lg-8" >
   <div class="panel panel-default">
       <div class="panel-heading">
@@ -41,11 +44,11 @@
               <div class="row">
                   <div class="col-lg-11">
                       <form role="form">
-                          <div class="form-group">
+                          <div class="col-xs-8">
                               <label>Name</label>
                               <input class="form-control">
                           </div>
-                          <div class="form-group">
+                          <div class="col-xs-4">
                               <label>Cell No.</label>
                               <input class="form-control" placeholder="Enter text">
                           </div>
@@ -64,7 +67,7 @@
       </div>
           <div class="panel-body">
               <div class="row">
-                  <div class="col-lg-11">
+                  <div class="col-lg-5">
                       <form role="form">
                         <div class="form-group input-group">
                             <span class="input-group-addon">Fare</span>
@@ -88,12 +91,32 @@
                         </div>
                       </form>
                     </div>
+
+                    <div class="col-lg-7">
+                        <form role="form">
+                            <div class="form-group">
+                                <label>Ticket By</label>
+                                <input class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Comment</label>
+                                <input class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Point</label>
+                                <input class="form-control">
+                            </div>
+                        </form>
+                      </div>
                   <!-- /.col-lg-6 (nested) -->
               </div>
               <!-- /.row (nested) -->
           </div>
       <!-- /.panel-body -->
   </div>
+
+  <button type="submit" class="btn btn-default">Submit Button</button>
+  <button type="reset" class="btn btn-default">Reset Button</button>
 </div>
 
 
@@ -123,6 +146,10 @@
                               <input class="form-control">
                           </div>
                           <div class="form-group">
+                              <label>Date</label>
+                              <input class="form-control">
+                          </div>
+                          <div class="form-group">
                               <label>Flown Date</label>
                               <input class="form-control">
                           </div>
@@ -136,43 +163,13 @@
       <!-- /.panel-body -->
   </div>
 
-  <div class="panel panel-default">
-      <div class="panel-heading">
-          Contributor Info
-      </div>
-          <div class="panel-body">
-              <div class="row">
-                  <div class="col-lg-11">
-                      <form role="form">
-                          <div class="form-group">
-                              <label>Ticket By</label>
-                              <input class="form-control">
-                          </div>
-                          <div class="form-group">
-                              <label>Comment</label>
-                              <input class="form-control">
-                          </div>
-                          <div class="form-group">
-                              <label>Point</label>
-                              <input class="form-control">
-                          </div>
-                      </form>
-                    </div>
-                  <!-- /.col-lg-6 (nested) -->
-              </div>
-              <!-- /.row (nested) -->
-          </div>
-      <!-- /.panel-body -->
-  </div>
 </div>
 
-<button type="submit" class="btn btn-default">Submit Button</button>
-<button type="reset" class="btn btn-default">Reset Button</button>
+
 
 </div
 </div>
-
-
+</div>
 
 
 
