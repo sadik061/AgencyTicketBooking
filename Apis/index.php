523694f41c8826a1d9d7458ea7f43099d8d00617
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2018-03-15 19:55:58 +0600

    Api Folder Direct Access Closed

---
+++ Apis/index.php
@@ -0,0 +1,32 @@
+<?php  session_start();?>
+
+<html>
+<head>
+    <title>You Fraud !!!</title>
+</head>
+<body>
+        What Are You Doing Here !!!
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abdullah Al Rifat
+ * Date: 15-Mar-18
+ * Time: 7:50 PM
+ */
+
+
+  if(!isset($_SESSION['loggedIn']))   // Checking whether the session is already there or not if
+      // true then header redirect it to the home page directly
+  {
+      echo '<script type="text/javascript"> window.open("../index.php","_self");</script>';            //  On Successful Login redirects to home.php
+      exit();
+      /* Redirect browser */
+
+  }
+
+
+?>
+
+
+</body>
+</html>
