790d3726f672d20001e5ed274599ba329a2408c6
sadik061 <sadik061@gmail.com>
2018-03-26 17:17:26 +0600

    Airlines page

---
+++ pages/Airlines.php
@@ -0,0 +1,263 @@
+<?php include '../pages/templates/head.html'; ?>
+
+<?php  session_start();
+
+?>
+
+<body>
+<div id="wrapper">
+    <?php
+
+    include '../pages/templates/headermenu.html';
+    include '../pages/templates/sidemenu.html';
+    include '../pages/templates/footer.html';
+    ?>
+
+    <div id="page-wrapper">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Ticket Info
+                    </div>
+                    <form role="form" method="post" onsubmit="load()">
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-11">
+                                    <div class="form-group">
+                                        <div class="form-group col-lg-4">
+                                            <label >From</label>
+                                            <input id="flownDateFrom" name="Flown_Date_From"  class="datepicker">
+                                        </div>
+                                        <div class="form-group col-lg-4">
+                                            <label >To</label>
+                                            <input id="flownDateTo" name="Flown_Date_To"  class="datepicker">
+                                        </div>
+                                    </div>
+                                    <!-- /.col-lg-6 (nested) -->
+                                </div>
+                                <!-- /.row (nested) -->
+                            </div>
+                            <!-- /.panel-body -->
+
+                            <button type="submit" name="insert_main_data" value="insert_main_data"  class="btn btn-default btn-primary">SUBMIT</button>
+                            <button type="reset" onclick="reset()" class="btn btn-default btn-primary">RESET</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>Novoair</h3>
+                            </div>
+                            <div class="col-lg-4">
+                                Total Sell:
+                            </div>
+                            <div class="col-lg-4">
+                                Amount:
+                            </div>
+                            <div class="col-lg-4">
+                            </div>
+                            <div class="col-lg-4">
+                                Total Capping :
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>PNR</th>
+                                    <th>Route</th>
+                                    <th>Time</th>
+                                    <th>Paid</th>
+                                    <th>Due</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>US-Bangla</h3>
+                            </div>
+                            <div class="col-lg-4">
+                                Total Sell:
+                            </div>
+                            <div class="col-lg-4">
+                                Amount:
+                            </div>
+                            <div class="col-lg-4">
+                            </div>
+                            <div class="col-lg-4">
+                                Total Capping :
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>PNR</th>
+                                    <th>Route</th>
+                                    <th>Time</th>
+                                    <th>Paid</th>
+                                    <th>Due</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+
+
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>Others</h3>
+                            </div>
+                            <div class="col-lg-4">
+                                Total Sell:
+                            </div>
+                            <div class="col-lg-4">
+                                Amount:
+                            </div>
+                            <div class="col-lg-4">
+                            </div>
+                            <div class="col-lg-4">
+                                Total Capping :
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>PNR</th>
+                                    <th>Route</th>
+                                    <th>Time</th>
+                                    <th>Paid</th>
+                                    <th>Due</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-6 -->
+
+        </div>
+        <!-- /.row -->
+
+
+
+    </div>
+</div>
+
+
+<?php include '../pages/templates/footer.html'; ?>
+
+
+
+</body>
+</html>
